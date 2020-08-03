00f6df32e5377f5b2efaf34b49da6d3d036bc0e0
Sangho.Nam <namdori61@gmail.com>
2020-08-03 13:27:09 +0900

    Model: Add KoBERT model class.

--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,8 @@
 import os, sys
 
 from model.bert_model import BertClassificationModel
+from model.kobert_model import KoBertClassficationModel
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-__all__ = ['BertClassificationModel']
+__all__ = ['BertClassificationModel','KoBertClassficationModel']
